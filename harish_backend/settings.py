de22acdbbe6d8d77f685d4f4549fdebf0bc9272e
manovatetechnologies-lab <manovatetechnologies@gmail.com>
2026-01-20 13:43:20 +0530

    Inital commit

--- harish_backend/settings.py
+++ harish_backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l9cg+h2u@$m7y*y=x63gl18-ls^9e(y3th*io2@w=geh842$f!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
